ca674a69767f4fb9ea7337014a046693fe3fe5ec
8wgf3b <karnabalaj@gmail.com>
2020-05-18 12:07:08 +0530

    Add logging

--- ingestion/dailyweb.py
+++ ingestion/dailyweb.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import logging
 
 
-logger = logging.getLogger('TS.webintakey')
+logger = logging.getLogger('TS.webintake')
 
 @lineprotocol
 class Weight(NamedTuple):
@@ -17,6 +17,7 @@ async def hello(request):
 async def load_wt(request):
     with open('htmls/wt.html') as f:
         content = f.read()
+    logger.info('Initial wt page load')
     return web.Response(text=content, content_type='text/html')
 
 async def wt(request):
@@ -24,4 +25,5 @@ async def wt(request):
     data = await request.post()
     wght = float(data['weight'])
     await client.write(Weight(wght))
+    logger.info(f'pushed {wght} kgs')
     return web.Response(text=f'{datetime.utcnow()}: {wght}')
--- webintake.py
+++ webintake.py
@@ -1,6 +1,7 @@
 from ingestion.dailyweb import hello, load_wt, wt
 from ingestion.post_snaps import init_db
 from aiohttp import web
+from logger import logger
 import asyncio
 
 if __name__ == '__main__':
@@ -10,9 +11,11 @@ if __name__ == '__main__':
               web.post('/wt', wt)]
 
     app.add_routes(routes)
+    logger.info('Adding routes')
     try:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(init_db('Lifestyle'))
     except (KeyboardInterrupt, SystemExit):
         logger.warning('Closing program...')
+    logger.info('Starting the web app')
     web.run_app(app)
